0a7fbcd65aac2a4b7c7407059dba7ac43e220374
Gearted <63016907+Gearted@users.noreply.github.com>
2022-06-30 13:45:39 +0200

    update

--- roulette/src/components/Page1/Container1.jsx
+++ roulette/src/components/Page1/Container1.jsx
@@ -24,46 +24,44 @@ function Container1() {
     setMustSpin(true);
   };
   return (
-    <div className="content-container1">
-      <article className="article1">
-        <div>
-          <p>texte de présentation de la roulette</p>
-        </div>
+    <article className="article1">
+      <div>
+        <h1>Faites vos jeux ! rien ne vas plus !</h1>
+        <h2>La chance vous dira ce que vous regarderez ce soir</h2>
+      </div>
 
-        <div className="container">
-          <div className="container1">
-            <button type="button" className="btn1">
-              Films
-            </button>
-            <button type="button" className="btn1">
-              Série
-            </button>
+      <div className="container1">
+        <button type="button" className="btn1">
+          Films
+        </button>
+        <button type="button" className="btn1">
+          Série
+        </button>
 
-            <select className="select" name="Genre" id="Genre">
-              <option value="">Drame</option>
-              <option value="">Comédie</option>
-              <option value="">Action</option>
-              <option value="">Thriller</option>
-              <option value="">Fantastique</option>
-              <option value="">Horreur</option>
-            </select>
-          </div>
-          <div className="roulette">
-            <Wheel
-              mustStartSpinning={mustSpin}
-              prizeNumber={prizeNumber}
-              data={data}
-              onStopSpinning={() => {
-                setMustSpin(false);
-              }}
-            />
-            <button className="btn1" onClick={handleSpinClick}>
-              GO
-            </button>
-          </div>
-        </div>
-      </article>
-    </div>
+        <select className="select" name="Genre" id="Genre">
+          <option value="">Drame</option>
+          <option value="">Comédie</option>
+          <option value="">Action</option>
+          <option value="">Thriller</option>
+          <option value="">Fantastique</option>
+          <option value="">Horreur</option>
+        </select>
+      </div>
+
+      <div className="roulette">
+        <Wheel
+          mustStartSpinning={mustSpin}
+          prizeNumber={prizeNumber}
+          data={data}
+          onStopSpinning={() => {
+            setMustSpin(false);
+          }}
+        />
+        <button className="btn1" onClick={handleSpinClick}>
+          GO
+        </button>
+      </div>
+    </article>
   );
 }
 
